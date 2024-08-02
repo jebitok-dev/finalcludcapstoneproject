f6bd3ddf0fb5cdbfa6107260ceea65a14e081b30
Jebitok <sharonjebitok@utu.io>
2024-08-02 23:30:07 +0300

    Add Readme and Changes to Main file

---
+++ README.md
@@ -0,0 +1,23 @@
+# Infrastructure as Code Project
+
+
+## SetUp Project Locally 
+
+`````
+$ git clone 
+$ cd 
+`````
+
+## Run/ Test Project 
+
+``````
+$ terraform init 
+$ terraform plan
+$ terraform apply 
+``````
+
+
+
+
+## Acknowledgement 
+I acknowledge [ALTSchool Africa](https://engineering.altschoolafrica.com/programs/cloud-engineering) for the Diploma in Cloud Engineering course that it has offered us over the last year and the quality instructors and resources that has helped us curate this project and skills we have gained.
--- main.tf
+++ main.tf
@@ -4,9 +4,18 @@ provider "aws" {
 
 resource "aws-rpc" "main" {
     cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
 
     tags = {
-        Name = "socks-shop-vpc"
+      Name = "socks-shop-vpc"
+    }
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "socks-shop-igw"
     }
 }
 
@@ -15,10 +24,30 @@ resource "aws-subnet" "public" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.${count.index}.0/24"
     availability_zone = data.aws_availability_zones.available.names[count.index]
+    map_public_ip_on_launch = true
 
     tags = {
         Name = "socks-shop-public-subnet-${count.index + 1}"
     }
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "socks-shops-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    count = 2
+    subnet_id = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public.id
 }
 
 data "aws_availability_zones" "available" {}
